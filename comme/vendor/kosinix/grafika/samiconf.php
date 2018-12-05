903b6da5ea03aa7bbeedab92cb8d53d1370bda4d
july <344151820@qq.com>
2018-12-05 14:57:24 +0800

    .

--- comme/vendor/kosinix/grafika/samiconf.php
+++
@@ -1,6 +0,0 @@
-<?php
-// To run in console type: php sami.phar update samiconf.php
-return new Sami\Sami('src', array(
-    'build_dir'            => __DIR__.'/api',
-    'cache_dir'            => __DIR__.'/sami-cache'
-));
